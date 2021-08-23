f3de41baf03b894036ac190a3dcca2c02daf6a03
quantsailor <quantsailor@gmail.com>
2021-08-23 10:05:50 +0900

    :commit

--- sailor_engine/__init__.py
+++ sailor_engine/__init__.py
@@ -1,2 +1,3 @@
-import numpy as np
-import pandas as pd
+import dataloader
+import backtest_engine
+import strategy
--- sailor_engine/strategy.py
+++ sailor_engine/strategy.py
@@ -1,6 +1,5 @@
-
-
-
+import numpy as np
+import pandas as pd
 
 class Strategy():
     def __init__(self):
