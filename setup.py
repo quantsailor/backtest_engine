fa5b0af58963b64481230dfff5afa1b39ec46436
quantsailor <quantsailor@gmail.com>
2021-08-23 10:27:35 +0900

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'sailor_engine',
+    name = 'backtest_engine',
     version = '0.0.1',
     author = "quantsailor",
     email = "quantsailor@gmail.com",
